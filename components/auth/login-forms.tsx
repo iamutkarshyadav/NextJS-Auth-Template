a8a7c11474d785ef7ba0c7a9e575564e44d2ecc6
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-03-23 23:06:29 +0530

    Add login form with validation

--- components/auth/login-forms.tsx
+++ components/auth/login-forms.tsx
@@ -1,5 +1,29 @@
+"use client";
+import * as z from "zod";
 import { CardWrapper } from "./card-wrapper";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
+import {Input} from "@/components/ui/input";
+import { 
+    Form,
+    FormControl,
+    FormLabel,
+    FormItem,
+    FormMessage, 
+    FormField
+} from "@/components/ui/form"
+
+import { useForm } from "react-hook-form";
+
+
 export const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "" ,  
+        },
+    });
     return (
         
         <CardWrapper
@@ -8,7 +32,41 @@ export const LoginForm = () => {
             backButtonHref="/auth/register"
             showSocial
         >
-            Login Form
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(() => { })}
+                className="space-y-6"
+                >
+                    <div className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel >Email</FormLabel>
+                                    <Input {...field}
+                                        placeholder="john.doe@example.com"
+                                        type="email"
+                                    />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel >Password</FormLabel>
+                                    <Input {...field}
+                                        placeholder="********"
+                                        type="password"
+                                    />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                </form> 
+            </Form>
         </CardWrapper>
     )
 }
+
